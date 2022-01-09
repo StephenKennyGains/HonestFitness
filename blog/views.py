ff30265169e085844a7dd75333bc3ce42d7d7734
StephenKennyGains <stephenkennygains@gmail.com>
2022-01-09 17:36:16 +0000

    Create general post view and base.html

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+
+class GeneralPostView(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1, category=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 3
+
