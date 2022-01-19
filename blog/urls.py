6ca08491cd55e9d80989c8103a52505c6e81a923
StephenKennyGains <stephenkennygains@gmail.com>
2022-01-19 18:56:13 +0000

    Add post detail view, template and urls. Update Post List to general

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home')
+    path('', views.PostListGeneral.as_view(), name='home'),
+    path('<slug:slug>/', views.PostFullView.as_view, name='post_full_view'),
 ]
