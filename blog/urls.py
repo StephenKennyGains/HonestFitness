b58c48be531682d2efb8e564425104b9ed6517ef
StephenKennyGains <stephenkennygains@gmail.com>
2022-01-19 18:58:11 +0000

    Fix post full view url and update page name correction

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostListGeneral.as_view(), name='home'),
-    path('<slug:slug>/', views.PostFullView.as_view, name='post_full_view'),
+    path('<slug:slug>/', views.PostFullView.as_view(), name='post_full_view'),
 ]
